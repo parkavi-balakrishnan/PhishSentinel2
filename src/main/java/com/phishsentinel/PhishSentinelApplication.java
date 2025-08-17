0c07c10ee9e7b42228ef76a40cdba0d47c78ad15
Aishwarya Prakash <aishwaryaaprakash88@gmail.com>
2025-08-17 21:54:28 +0530

    Initial commit - setup Spring Boot backend

---
+++ src/main/java/com/phishsentinel/PhishSentinelApplication.java
@@ -0,0 +1,12 @@
+package com.phishsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhishSentinelApplication.class, args);
+    }
+}
